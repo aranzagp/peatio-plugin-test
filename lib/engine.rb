cb2e7cb067ca9911e036806dfd27e87c3ef2309b
aranza <aranza@michelada.io>
2018-04-05 12:17:45 -0500

    First commit

---
+++ lib/engine.rb
@@ -0,0 +1,20 @@
+module MyPlugin
+  VERSION = '1.0.0'
+
+  class Engine < Rails::Engine
+    isolate_namespace MyPlugin
+
+    # Ensure plugin is compatible with Peatio.
+    config.before_initialize do
+      if Peatio::VERSION < '1.4'
+        Kernel.abort "This plugin is designed to work only on Peatio 1.4.x. You have #{Peatio::VERSION}."
+      end
+    end
+
+    # Mount new API resource in hook (friendly with development environment).
+    # More about configuration stages: http://guides.rubyonrails.org/configuring.html#configuring-action-dispatch
+    config.to_prepare do
+      APIv2::Mount.mount APIv2::Alphabet
+    end
+  end
+end
